466f5ef93b7bd3f99b4b99c9dd3bcacad41bc59a
Frankwotfurters <59547088+Frankwotfurters@users.noreply.github.com>
2021-06-30 12:45:08 +0800

    format

--- deepfakedetect.py
+++ deepfakedetect.py
@@ -14,16 +14,14 @@ bestResolutionVideo.download("downloadedvid.mp4")
 cap = cv2.VideoCapture("downloadedvid.mp4")
 while(True):
     #Capture frame-by-frame
-   ret, frame = cap.read()
+    ret, frame = cap.read()
     #Our operations on the frame come here
-   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #Display the resulting frame
-   cv2.imshow('frame',gray)
-   if cv2.waitKey(1) & 0xFF == ord('q'):
-       break
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 #When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-
-
-
