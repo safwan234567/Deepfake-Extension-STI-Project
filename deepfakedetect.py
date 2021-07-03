b2521f34af23615b180e6adf56f6ec17003d2df6
Frankwotfurters <59547088+Frankwotfurters@users.noreply.github.com>
2021-07-03 17:56:21 +0800

    extension tests

--- deepfakedetect.py
+++ deepfakedetect.py
@@ -7,6 +7,8 @@ import pandas as pd
 import glob2
 from pathlib import Path
 from mtcnn.mtcnn import MTCNN
+from skimage import measure
+from skimage.metrics import structural_similarity as ssim
 # from skimage import measure
 
 # reading video fame
@@ -39,13 +41,43 @@ def extract_multiple_videos(intput_filenames):
     cv2.waitKey(50) #Wait 50msec
     cap.release()
 
-url = input("Paste video link here: ")
-# create video object
-video = pafy.new(url)
-# extract information about best resolution video available 
-bestResolutionVideo = video.getbest()
-# download the video as downloadedvid.mp4
-bestResolutionVideo.download("downloadedvid.mp4")
+def mse(imageA, imageB):
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return err
+
+def compare_images(imageA, imageB, title):
+    # compute the mean squared error and structural similarity
+    # index for the images
+    m = mse(imageA, imageB)
+    s = ssim(imageA, imageB)
+    # setup the figure
+    fig = plt.figure(title)
+    plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+    # show first image
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(imageA, cmap = plt.cm.gray)
+    plt.axis("off")
+    # show the second image
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(imageB, cmap = plt.cm.gray)
+    plt.axis("off")
+    # show the images
+    plt.show()
+
+if False:
+    url = input("Paste video link here: ")
+    # create video object
+    video = pafy.new(url)
+    # extract information about best resolution video available 
+    bestResolutionVideo = video.getbest()
+    # download the video as downloadedvid.mp4
+    bestResolutionVideo.download("downloadedvid.mp4")
 
 cap = cv2.VideoCapture("downloadedvid.mp4")
 # while(True): Commented for now since it doesnt work
@@ -57,6 +89,37 @@ cap = cv2.VideoCapture("downloadedvid.mp4")
 #     cv2.imshow('frame',gray)
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
+
+original = cv2.imread("./real/maxresdefault.jpg")
+shopped = cv2.imread("./frames/1.jpg")
+# convert the images to grayscale
+original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+shopped = cv2.cvtColor(shopped, cv2.COLOR_BGR2GRAY)
+
+# initialize the figure
+fig = plt.figure("Images")
+images = ("Original", original), ("modified", shopped)
+# loop over the images
+for (i, (name, image)) in enumerate(images):
+    # show the image
+    ax = fig.add_subplot(1, 3, i + 1)
+    ax.set_title(name)
+    plt.imshow(image, cmap = plt.cm.gray)
+    plt.axis("off")
+# show the figure
+plt.show()
+# compare the images
+compare_images(original, original, "Original vs. Original")
+compare_images(original, shopped, "Original vs. Modified")
+# cv2.subtract(original)
+# img3 = original-shopped
+image3 = cv2.absdiff(original, shopped)
+image3
+is_all_zero = not np.any(image3)
+if is_all_zero:
+    print('Array contains only 0')
+else:
+    print('Array has non-zero items too')
 
 #When everything done, release the capture
 cap.release()
---
+++ extension/popup.js
@@ -0,0 +1,33 @@
+// Initialize button with user's preferred color
+let click = document.getElementById("click");
+
+// When the button is clicked
+click.addEventListener("click", async () => {
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+    console.log(url);
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+          console.log(xhr.responseText);
+      }
+  }
+    xhr.open("POST", 'https://api.funtranslations.com/translate/yoda.json', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    console.log(JSON.stringify({
+      text: "Hello mum."
+  }));
+    xhr.send(data=JSON.stringify({
+        text: "Hello mum."
+    }));
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
