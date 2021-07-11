d196130f99b96d59a314acfbf80e2bbd448ff403
Safwan <40573918+safwan234567@users.noreply.github.com>
2021-07-12 06:40:20 +0800

    Automatically send detected link to API
    - send detected link to /url/scan API GET request
    - fixed issue so pop up detects link for any tab its on and not just new tab

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,20 @@
-chrome.storage.local.get(["dfe"], (result) => {
-  document.getElementById("url").innerHTML = result.dfe.url;
-});
+const http = new XMLHttpRequest()
+
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+   function(tabs){
+      document.write(tabs[0].url);
+	  let apiLink = tabs[0].url
+	  http.open("GET", "https://api.deepware.ai/api/v1/url/scan?video-url=" + apiLink)
+
+		http.setRequestHeader("X-Deepware-Authentication", "067c7e83-0036-4c36-9c98-d2613e42b9e5")            
+
+		http.send()
+
+		http.onload = () => console.log(http.responseText)
+		
+   }
+
+   
+);
+
+
