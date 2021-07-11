3e0e164e4c82b99801e19ac8ba0287856741f716
Safwan <40573918+safwan234567@users.noreply.github.com>
2021-07-12 07:48:52 +0800

    added functions to display results automatically
    - report id is parsed from json response then used to generate the API link to get the deepfake results
    - timer is added to ensure that results complete being computed before being requested

--- extension/popup.js
+++ extension/popup.js
@@ -9,12 +9,37 @@ chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}
 		http.setRequestHeader("X-Deepware-Authentication", "067c7e83-0036-4c36-9c98-d2613e42b9e5")            
 
 		http.send()
+		var fakeDetection = function(fakeDetection){
+		
+		const myJSON = http.responseText;
+		http.onload = () => console.log(myJSON)
+		console.log(myJSON)
+		const reportCheck = JSON.parse(myJSON);
+		reportID = reportCheck["report-id"];
+		console.log("Report id = " + reportID)
+		// sleep time expects milliseconds
+		function sleep (time) {
+		return new Promise((resolve) => setTimeout(resolve, time));
+		}
 
-		http.onload = () => console.log(http.responseText)
+	// Usage!
+		sleep(50).then(() => {
+			// Do something after the sleep!
+			http.open("GET", "https://api.deepware.ai/api/v1/video/report?report-id=" + reportID)
+			http.setRequestHeader("X-Deepware-Authentication", "067c7e83-0036-4c36-9c98-d2613e42b9e5")            
+
+			http.send()
+
+			http.onload = () => console.log(http.responseText)
+		});
+		}
+			
+		http.onload = () => fakeDetection()
 		
    }
 
    
 );
+
 
 
