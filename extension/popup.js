3c4f3e1ecd733f7245ac1329cb237e35c513561c
Safwan <40573918+safwan234567@users.noreply.github.com>
2021-07-12 17:42:47 +0800

    Changed from document.write to innerHTML for link

--- extension/popup.js
+++ extension/popup.js
@@ -2,7 +2,12 @@ const http = new XMLHttpRequest()
 
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
    function(tabs){
-      document.write(tabs[0].url);
+      document.getElementById("url").innerHTML = (tabs[0].url);
+
+    function buttonClick() {
+    document.getElementById("click").innerHTML = "clicked";
+	}
+		
 	  let apiLink = tabs[0].url
 	  http.open("GET", "https://api.deepware.ai/api/v1/url/scan?video-url=" + apiLink)
 
